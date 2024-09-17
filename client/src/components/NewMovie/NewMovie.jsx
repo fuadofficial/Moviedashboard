883a7c5a7f963a750d7af17a80f68e3b8ec471d3
fuad <ahmadfuadoorakam@gmail.com>
2024-09-17 20:20:52 +0530

    created new movie upload page styles

--- client/src/components/NewMovie/NewMovie.jsx
+++ client/src/components/NewMovie/NewMovie.jsx
@@ -6,12 +6,61 @@ const NewMovie = () => {
     return (
         <div className='newmovie-container'>
             <div className="upload-image">
-                <label htmlFor>Image</label>
-                <input type='file' />
+                <label className='lable-setup'>Image</label>
+                <input className='texts' type='file' />
             </div>
-            <div className="upload-image">
-                <label htmlFor>Image</label>
-                <input type='file' />
+            <div className="title">
+                <label className='lable-setup' >Title</label>
+                <input className='texts' type='text' placeholder='Type here' />
+            </div>
+            <div className="description">
+                <label className='lable-setup' >Description</label>
+                <input className='texts' type='text' placeholder='Description' />
+            </div>
+            <div className="rating">
+                <label className="label">Rating</label>
+                <input
+                    type="range"
+                    // value={rating}
+                    step="25"
+                // onChange={handleRatingChange}
+                />
+                <div className="nubmers">
+                    <span>1</span>
+                    <span>2</span>
+                    <span>3</span>
+                    <span>4</span>
+                    <span>5</span>
+                </div>
+            </div>
+            <div className="tick-box-container">
+                <div className="tick-box">
+                    <label>Action</label>
+                    <input type="checkbox" className='tick-box' />
+                </div>
+                <div className="tick-box">
+                    <label>Action</label>
+                    <input type="checkbox" className='tick-box' />
+                </div>
+                <div className="tick-box">
+                    <label>Action</label>
+                    <input type="checkbox" className='tick-box' />
+                </div>
+                <div className="tick-box">
+                    <label>Action</label>
+                    <input type="checkbox" className='tick-box' />
+                </div>
+                <div className="tick-box">
+                    <label>Action</label>
+                    <input type="checkbox" className='tick-box' />
+                </div>
+                <div className="tick-box">
+                    <label>Action</label>
+                    <input type="checkbox" className='tick-box' />
+                </div>
+            </div>
+            <div className="button">
+                <button type='submit'>Submit</button>
             </div>
         </div>
     );
