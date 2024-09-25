d2f4c293dee735ac3adfc76da4b77a8578bf17f7
unknown <ahmadfuadoorakam@gmail.com>
2024-09-25 04:54:37 -0700

    added some changes for fix error

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDb = async () => {
     try {
-        const { connection } = await mongoose.connect(process.env.MONGO_URL || 'mongodb://mongodb://0.0.0.0:27017/movie');
+        const { connection } = await mongoose.connect(process.env.MONGO_URL);
         console.log("Connected to MongoDB at: " + connection.host);
     } catch (error) {
         console.log("Connection error:", error);
