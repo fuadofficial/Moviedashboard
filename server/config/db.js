f191613fa304f3a408637c43be1b7972b292e8a1
unknown <ahmadfuadoorakam@gmail.com>
2024-09-24 20:06:39 -0700

    i try many method for fix my mongoose connection problem . but not fix that problem

--- server/config/db.js
+++ server/config/db.js
@@ -1,12 +1,13 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const connectDb = async () => {
     try {
-        const { connection } = await mongoose.connect(process.env.MONGO_URL);
-        console.log("data connected..." + connection.host);
+        const { connection } = await mongoose.connect(process.env.MONGO_URL || 'mongodb://mongodb://0.0.0.0:27017/movie');
+        console.log("Connected to MongoDB at: " + connection.host);
     } catch (error) {
-        console.log(error);
+        console.log("Connection error:", error);
     }
 }
 
 module.exports = connectDb;
+
