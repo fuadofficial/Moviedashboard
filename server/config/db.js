3aef0dcb599d9a0e3592f9442d4580aa72461f79
unknown <ahmadfuadoorakam@gmail.com>
2024-10-01 08:44:21 -0700

    fixing error of mongo db atls connections

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDb = async () => {
     try {
-        const { connection } = await mongoose.connect(process.env.MONGO_URL);
+        const { connection } = await mongoose.connect("mongodb+srv://ahmadfuadoorakam:xPkViPFz5bW4GlTj@cluster0.10tdw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
         console.log("Connected to MongoDB at: " + connection.host);
     } catch (error) {
         console.log("Connection error:", error);
