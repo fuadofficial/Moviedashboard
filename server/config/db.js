a20d36d643dfca771ebbf58ed9479c9d3869869b
unknown <ahmadfuadoorakam@gmail.com>
2024-10-07 18:30:47 -0700

    fix error

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDb = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/movie');
+        await mongoose.connect(process.env.MONGO_URL);
         console.log('MongoDB connected');
     } catch (error) {
         console.error('Connection error:', error);
